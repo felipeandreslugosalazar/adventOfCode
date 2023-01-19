8c94b70c7fb36c593f75eb1ee0f46c91c65765c8
Meta Dev <felipendreslugo@gmail.com>
2023-01-18 23:57:27 -0500

    rename spec file

--- 2015/day01/spec/day01_spec.rb
+++ 2015/day01/spec/day01_spec.rb
@@ -1,7 +1,8 @@
 require "where_should_santa_go"
+require "at_what_step_goes_santa_to_the_basement"
 
-describe "Examples" do
-  context "Part 01" do
+describe "Day 01" do
+  context "Part 01 - where should santa go ?" do
     it "(()) and ()() take Santa to floor 0" do
       expect(where_should_santa_go("(())")).to eq(0)
       expect(where_should_santa_go("()()")).to eq(0)
@@ -21,6 +22,16 @@ describe "Examples" do
     it "))) and )())()) take Santa to floor -3" do
       expect(where_should_santa_go(")))")).to eq(-3)
       expect(where_should_santa_go(")())())")).to eq(-3)
+    end
+  end
+
+  context "Part 02 - At what step goes santa to the basement ?" do
+    it ") causes Santa to enter the basement at character position 1" do
+      expect(position_of_the_character(")")).to eq(1)
+    end
+
+    it "()()) causes him to enter the basement at character position 5" do
+      expect(position_of_the_character("()())")).to eq(5)
     end
   end
 end
