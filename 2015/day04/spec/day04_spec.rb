cd92cd342cd65ca8c2375b6f19dd7fa2c4a7ebff
Meta Dev <felipendreslugo@gmail.com>
2023-01-27 21:59:35 -0500

    day 04 - specs - edit it lines

--- 2015/day04/spec/day04_spec.rb
+++ 2015/day04/spec/day04_spec.rb
@@ -2,11 +2,11 @@ require "md5"
 
 describe "Day 04" do
   context "Part 01 - what is the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash?" do
-    it "for abcdef hash 2nd part is 609043" do
+    it "for abcdef hash 2nd part is 609043 => abcdef609043" do
       expect(md5_to_match("abcdef")).to eq("609043")
     end
 
-    it "for pqrstuv hash 2nd part is 1048970" do
+    it "for pqrstuv hash 2nd part is 1048970 => pqrstuv1048970" do
       expect(md5_to_match("pqrstuv")).to eq("1048970")
     end
   end
