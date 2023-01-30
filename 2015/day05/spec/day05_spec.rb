95f24a20872825faeb341f00da80b73838b969e3
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2023-01-30 19:40:25 +0100

    day 02 - part 01 - edit specs

--- 2015/day05/spec/day05_spec.rb
+++ 2015/day05/spec/day05_spec.rb
@@ -4,27 +4,26 @@ require "is_string_nice"
 
 describe "--- Day 5: Doesn't He Have Intern-Elves For This? ---" do
   context "Part 01 -How many strings are nice?" do
+    # ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
+    # aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
+    # jchzalrnumimnmhp is naughty because it has no double letter.
+    # haegwjzuvuyypxyu is naughty because it contains the string xy.
+    # dvszwmarrgswjxmb is naughty because it contains only one vowel.
+
     it "ugknbfddgicrmopn is nice" do
       expect(is_string_nice?("ugknbfddgicrmopn")).to be true
     end
     it "aaa is nice" do
       expect(is_string_nice?("aaa")).to be true
     end
-    it "jchzalrnumimnmhp is nice" do
+    it "jchzalrnumimnmhp is naughty" do
       expect(is_string_nice?("jchzalrnumimnmhp")).to be false
     end
-    it "haegwjzuvuyypxyu is nice" do
+    it "haegwjzuvuyypxyu is naughty" do
       expect(is_string_nice?("haegwjzuvuyypxyu")).to be false
     end
-    it "dvszwmarrgswjxmb is nice" do
+    it "dvszwmarrgswjxmb is naughty" do
       expect(is_string_nice?("dvszwmarrgswjxmb")).to be false
     end
-
-    #
-    # ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
-    # aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
-    # jchzalrnumimnmhp is naughty because it has no double letter.
-    # haegwjzuvuyypxyu is naughty because it contains the string xy.
-    # dvszwmarrgswjxmb is naughty because it
   end
 end
