98a2fe8d3912c6249381e14da7d7f9f844fbdf43
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2023-02-02 18:46:21 +0100

    day 05 - part 01 - update main def

--- 2015/day05/lib/is_string_nice.rb
+++ 2015/day05/lib/is_string_nice.rb
@@ -1,9 +1,18 @@
-# require_relative "is_string_nice"
+require_relative "input"
+require_relative "helper"
 
 # PART 1.
 
 # input array of strings
-def is_string_nice?(input)
+def how_many_strings_are_nice(input)
+  input.split("\n").map! { |string| is_string_nice?(string) ? 1 : 0 }.sum
 end
 
-puts "How many strings are nice? => " + is_string_nice?(@elf_instructions).to_s
+# puts "*****************************************"
+# puts "***          DAY 05 - PART 01         ***"
+# puts "*****************************************"
+# puts "*** How many strings are nice? => " + how_many_strings_are_nice(@nice_naughty_list).to_s + " ***"
+# puts "*****************************************"
+
+puts "How many strings are nice? => " + how_many_strings_are_nice(@nice_naughty_list).to_s
+# => 238
