675ba21f0f68a4952109c5867ef67c3fd79ad7bd
Meta Dev <felipendreslugo@gmail.com>
2023-02-15 21:39:12 +0100

    varios

---
+++ varios.rb
@@ -0,0 +1,32 @@
+# def percentage_completed(array, iteration)
+#   @percentage = (100.to_f / (@instructions_for_the_ideal_lighting_configuration.length.to_f)).round(2)
+#   print "\e[2J\e[f"
+#   print "\r#{@percentage * iteration}" + "% completed"
+#   # sleep(0.5)
+#   print "\e[2J\e[f" if iteration + 1 >= @instructions_for_the_ideal_lighting_configuration.length
+#   print "\r100% completed" if iteration + 1 >= @instructions_for_the_ideal_lighting_configuration.length
+# end
+
+# print "11MB 294K/s"
+# sleep(1)
+# print "\r"
+# sleep(1)
+# print "12MB 307K/s"
+# sleep(1)
+
+# print "." + ".".red + "........"
+# sleep(1)
+# print "\r"
+# print "..." + ".".red + "......"
+# sleep(1)
+
+# ("a".."z").to_a.each do |char|
+#   print "\r"
+#   print char
+#   sleep(0.5)
+# end
+
+# (1..10).each do |percent|
+#   print "\r#{percent * 10}% complete"
+#   sleep(0.5)
+# end
