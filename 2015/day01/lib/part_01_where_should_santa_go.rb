45eefbf7e1b0fa3db1c132720f5cd22f18536550
Meta Dev <felipendreslugo@gmail.com>
2023-12-27 10:21:06 -0500

    add new solution for 2015 day 01 part 01

--- 2015/day01/lib/part_01_where_should_santa_go.rb
+++ 2015/day01/lib/part_01_where_should_santa_go.rb
@@ -5,6 +5,7 @@ require_relative "input"
 # To what floor do the instructions take Santa?
 
 def where_should_santa_go(input)
+  @time_start = Time.now
   input = input.split("")
   # # start at ground floor
   sum = 0
@@ -14,8 +15,22 @@ def where_should_santa_go(input)
     direction = (instruction == "(" ? 1 : -1)
     sum = sum + direction
   end
+  @time_end = Time.now
+  puts "total time = #{@time_end - @time_start}"
   sum
+end
+
+def where_should_santa_go_27_12_20023(input)
+  @time_start = Time.now
+  input1 = @instructions.chars.select { |e| e == "(" }.count
+  input2 = @instructions.chars.select { |e| e == ")" }.count
+  @answer = input1 > input2 ? input1 - input2 : input1 + input2
+  @time_end = Time.now
+  puts "total time = #{@time_end - @time_start}"
+  return @answer
 end
 
 # =>
 puts "To what floor do the instructions take Santa? " + where_should_santa_go(@instructions).to_s
+# =>
+puts "To what floor do the instructions take Santa? " + where_should_santa_go_27_12_20023(@instructions).to_s
