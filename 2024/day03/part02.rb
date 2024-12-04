d8b16103ea9c74e888dedabac41d6d1f97ba1d31
felipe andres lugo salazar <felipeandreslugo@gmail.com>
2024-12-04 20:50:57 +0100

    day3

--- 2024/day03/part02.rb
+++ 2024/day03/part02.rb
@@ -1,12 +1,15 @@
 #set the input
 file_path = ("./input.txt") # CHALLENGE
-file_path = ("./input_example1.txt") # EXAMPLE
+file_path = ("./input_example2.txt") # EXAMPLE
 # open the file
 file = File.open(file_path)
 # read the file
 input_text = file.read
 
 @instructions = input_text
+  .split(/do/)
+  .reject { |line| line.start_with?("n't") }
+  .join
   .scan(/mul\({1}\d{1,3},\d{1,3}\){1,3}/)
   .map! { |instruction| instruction.gsub(/[mul(), ]/, " ").strip.split }
   .map! { |first_num, last_num| [first_num.to_i, last_num.to_i] }
